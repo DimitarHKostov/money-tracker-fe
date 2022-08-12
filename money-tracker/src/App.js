87ac310f332be980f2e2bdbc79e65a3de4009d61
Dimitar Kostov <dimitarkostov@Dimitars-MacBook-Pro.local>
2022-08-13 00:09:05 +0300

    react register extend

--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -1,9 +1,5 @@
-
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { Link } from "react-router-dom";
-
 const App = () => {
-  return (<h1></h1>)
+  return (<></>)
 }
 
 export default App;
